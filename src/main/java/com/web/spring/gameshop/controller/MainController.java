1a170912f99a0d8ab4b168788f6e2fe626b2d3df
shdwraze <dedmoped1997@gmail.com>
2022-05-31 02:56:37 +0300

    Add register users

--- src/main/java/com/web/spring/gameshop/controller/MainController.java
+++ src/main/java/com/web/spring/gameshop/controller/MainController.java
@@ -1,10 +1,23 @@
 package com.web.spring.gameshop.controller;
 
+import com.web.spring.gameshop.entity.Role;
+import com.web.spring.gameshop.entity.User;
+import com.web.spring.gameshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping({"/", ""})
     public String index() {
@@ -16,4 +29,20 @@ public class MainController {
         return "login";
     }
 
+    @GetMapping("/register")
+    public String register(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String addNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+
+        return "redirect:/";
+    }
 }
